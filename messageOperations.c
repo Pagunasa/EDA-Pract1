3b982cd93b111f9b19d8b2faf18f06bafbf70a38
u140172 <u140172@54005p11.ds.upf.edu>
2017-01-24 13:16:54 +0100

    New Functions od folders and messages

--- messageOperations.c
+++ messageOperations.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+char showMsg(int msgID){
+    return "Show Message";
+}
+
+char deleteMsg(int msgID){
+    return "Delete Message";
+}
+
+char createMessage(/*struct Mensaje*/){
+    return "Create Message";
+}
+
+char moveMsg(int msgId, char folderName[50]){
+    return "Move Message";
+}
+
+char searchInMsg(char searchParam[100]){
+    return "Search in Message";
+}
+
+char deleteMsgInFolder(int msgID, char folderName[50]){
+    
+}
